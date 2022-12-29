4b6c2e435f70c16e9cb49a11b371658d57fdc8e4
Alan Hertanto Guspermana <alanhertantog@gmail.com>
2022-12-29 20:23:25 +0700

    Update CharacterStatBehaviour.cs

--- Assets/Scripts/Character/CharacterStatBehaviour.cs
+++ Assets/Scripts/Character/CharacterStatBehaviour.cs
@@ -7,7 +7,7 @@ public class CharacterStatBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Woy!");
     }
 
     // Update is called once per frame
